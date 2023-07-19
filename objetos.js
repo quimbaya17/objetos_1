1f4bf06a29fbec34ca62b53b4e5e9eeb6f823415
ferny461 <ferneyguzman461@gmail.com>
2023-07-18 20:50:22 -0500

    creacion de objetos

---
+++ objetos.js
@@ -0,0 +1,25 @@
+administrador={
+    id:1,
+    nombre:"juan",
+    edad:13,
+    direccion:"calle 4 n 2-4",
+    telefono:"3102265656",
+
+}
+mandadero={
+    id:2,
+    nombre:"carlos",
+    edad:32,
+    direccion:"calle 5 n 2-4",
+    telefono:"3102265656",
+
+}
+
+usuario={
+    id:3,
+    nombre:"nelly",
+    edad:28,
+    direccion:"calle 4 n 2-4",
+    telefono:"3202265656",
+
+}
